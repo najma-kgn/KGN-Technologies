6ac2686d8cbdddb61916038e581480c4a98ade1a
najma-kgn <najma.sodha@kgn-software.com>
2026-04-28 16:53:58 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,24 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1138189866'), // Directly set the JWT secret
+    },
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
